70af8ece9f98cb013bceaccc401309cd27faa57d
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-07 13:20:39 -0700

    add 1.2 exercise

--- part1/courseinfo/courseinfo/src/index.js
+++ part1/courseinfo/courseinfo/src/index.js
@@ -3,43 +3,55 @@ import ReactDOM from 'react-dom'
 
 //TODO: refactor Executing component into a function
 
+const logging = true
+
+function log(message, type) {
+	if ((type === "execution") && (logging))
+		console.log("Executing " + message + " component");
+}
+
 const Header = (props) => {
-	console.log("Executing Header component")
+	log("Header", "execution")
 	return (
 		<h1>{props.course}</h1>
 	)
 }
 
-const Content = (props) => {
-	console.log("Executing Content component")
-	return (
-		<p>{props.part} {props.exercises}</p>
-	)
-}
-
-const Total = (props) => {
-	console.log("Executing Total component")
+const Part = (props) => {
+	log("Part", "execution")
 	return (
 		<p>Number of exercises {props.number}</p>
 	)
 }
 
+const Content = (props) => {
+	log("Content", "execution")
+	return (
+		<div>
+			<Part part={props.part[0]} number={props.exercises[0]} />
+			<Part part={props.part[1]} number={props.exercises[1]} />
+			<Part part={props.part[2]} number={props.exercises[2]} />
+		</div>
+	)
+}
+
+const Total = (props) => {
+	log("Total", "execution")
+	return (
+		<p>Number of exercises: {props.number}</p>
+	)
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exercises = [10, 7, 14]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total number={exercises1 + exercises2 + exercises3} />
+      <Content part={parts} exercises={exercises} />
+      <Total number={exercises[0] + exercises[1] + exercises[2]} />
     </div>
   )
 }
