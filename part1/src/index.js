38eef0e1daaa121984cde5d6d48ea8db75dad79c
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-09 09:30:00 -0700

    finished part 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -29,25 +29,92 @@ const App = () => {
 */
 
 const App = (props) => {
+	const [value, setValue] = useState(10)
+
+	const setToValue = (newValue) => () => {
+		setValue(newValue)
+	}
+
+	return (
+		<div>
+			{value}
+			<button onClick={setToValue(1000)}>thousand</button>
+			<button onClick={setToValue(0)}>zero</button>
+			<button onClick={setToValue(value + 1)}>increment</button>
+		</div>
+	)
+}
+
+/*
+const Display = ({ counter }) => {
+	return (
+		<p>{counter}</p>
+	)
+}
+
+const Button = (props) => {
+	return (
+		<button onClick={props.handleClick}>
+			{props.text}
+		</button>
+	)
+}
+
+const History = (props) => {
+	if (props.allClicks.length === 0) {
+		return (
+			<div>
+				the app is used by pressing the buttons
+			</div>
+		)
+	}
+
+	return (
+		<div>
+			button press history: {props.allClicks.join(' ')}
+		</div>	
+	)
+}
+
+
+const App = (props) => {
 	const [ counter, setCounter ]  = useState(0)
+	const [left, setLeft] = useState(0)
+	const [right, setRight] = useState(0)
+	const [allClicks, setAll] = useState([])
+
+	const handleLeftClick = () => {
+		setAll(allClicks.concat('L'))
+		setLeft(left + 1)
+	}
+
+	const handleRightClick = () => {
+		setAll(allClicks.concat('R'))
+		setRight(right + 1)
+	}
 		
 	const increaseByOne = () => setCounter(counter + 1)
+	const decreaseByOne = () => setCounter(counter - 1)
 	const setToZero = () => setCounter(0)
 
 	return (
 		<div>
-			{counter}
+			{left}
+			<Button 
+				handleClick={handleLeftClick}
+				text='left'
+			/>
+			<Button 
+				handleClick={handleRightClick}
+				text='right'
+			/>
+			{right}
 			<br />
-			<button onClick={increaseByOne} >
-				plus
-			</button>
-			<button onClick={setToZero}>
-				zero
-			</button>
+			<History allClicks={allClicks} />
 		</div>
 	)	
 }
 
-let counter = 1
+let counter = 1*/
 
-ReactDOM.render(<App counter={counter}/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
