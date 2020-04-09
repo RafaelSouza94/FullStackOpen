531a395e9274073426345ade490b0139c91a484a
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-09 12:23:18 -0700

    add exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -33,8 +33,18 @@ const DisplayPct = ({value}) => {
 	)
 }
 
-
-
+const Statistics = ({good, good_name, neutral, neutral_name, bad, bad_name, total, total_name}) => {
+	return(
+		<div>
+			<DisplayStat feedback={good_name} value={good} />
+			<DisplayStat feedback={neutral_name} value={neutral} />
+			<DisplayStat feedback={bad_name} value={bad} />
+			<DisplayStat feedback={total_name} value={total} />
+			<DisplayAvg value={(good - bad) / total} />
+			<DisplayPct value={(good * 100) / total} />
+		</div>
+	)
+}
 
 const App = () => {
 	const [good, setGood] = useState(0)
@@ -70,15 +80,20 @@ const App = () => {
 			<Button handleClick={handleNeutral} text={neutral_name} />
 			<Button handleClick={handleBad} text={bad_name} />
 			<Header text={stats_text} />
-			<DisplayStat feedback={good_name} value={good} />
-			<DisplayStat feedback={neutral_name} value={neutral} />
-			<DisplayStat feedback={bad_name} value={bad} />
-			<DisplayStat feedback={total_name} value={total} />
-			<DisplayAvg value={(good - bad) / total} />
-			<DisplayPct value={(good * 100) / total} />
+			<Statistics 
+				good={good} 
+				good_name={good_name}
+				neutral={neutral}
+				neutral_name={neutral_name}
+				bad={bad}
+				bad_name={bad_name}
+				total={total}
+				total_name={total_name}
+			/>  
 		</div>
 	)
-	}
+}
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
