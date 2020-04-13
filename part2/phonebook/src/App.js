29e1912032ba68ce36e1cb692fd7e70874cc90f8
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-13 10:46:23 -0700

    add exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react'
 import Display from './components/Display.js'
 
 const App = () => {
-  const [ persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState('')
+
+
 
   const addName = (event) => {
     event.preventDefault()
@@ -30,21 +38,33 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  const personsToShow = newSearch === '' ? persons : persons.filter(
+    person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      Search: <input value={newSearch} onChange={handleSearchChange} />
+      <br />
+      <h2>Add New Name</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          Name: <input value={newName} onChange={handleNameChange} />
           <br />
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          <br />
+          Number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
+        <br />
         <div>
           <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {personsToShow.map(person => 
         <Display key={person.name} name={person.name} number={person.number} />
       )}
     </div>
