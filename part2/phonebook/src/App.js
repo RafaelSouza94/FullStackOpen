b7e3a17dacf265381276e381dec3590d74dc5755
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-13 10:26:26 -0700

    add exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import Display from './components/Display.js'
 
 const App = () => {
-  const [ persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [ persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     if(persons.filter(person => person.name === newName).length === 0){
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personObject))
     }
@@ -17,10 +19,15 @@ const App = () => {
       alert(`${newName} is already in the phonebook!`)
     }
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -28,7 +35,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit" onClick={addName}>add</button>
@@ -36,7 +45,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map(person => 
-        <Display key={person.name} name={person.name} />
+        <Display key={person.name} name={person.name} number={person.number} />
       )}
     </div>
     )
--- part2/phonebook/src/components/Display.js
+++ part2/phonebook/src/components/Display.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Display = ({name}) => {
+const Display = ({name, number}) => {
 	return(
-		<p>{name}</p>
+		<p><b>{name}:</b> {number}</p>
 	)
 }
 
