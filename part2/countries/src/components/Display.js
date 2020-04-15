298bc2b7a636e8365080cebb772e3d4898751a10
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-15 10:26:32 -0700

    add exercise 2.14

--- part2/countries/src/components/Display.js
+++ part2/countries/src/components/Display.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const weather_api_key = process.env.REACT_APP_WEATHERSTACK_API_KEY
+const weather_url = `http://api.weatherstack.com/current?access_key=${weather_api_key}&query=`
 
 const PrintCountry = ({country}) => {
+	console.log("Reached PrintCountry with ", country)
+	const [weather_temperature, setTemperature] = useState('')
+	const [wind_speed, setWindSpeed] = useState('')
+	const [weather_icon, setWeatherIcon] = useState('')
+	const [wind_direction, setWindDirection] = useState('')
+
+	const hook = () => {
+		axios
+			.get(weather_url.concat(country.name))
+			.then(response => {
+				console.log(response.data)
+				setTemperature(response.data['current'].temperature)
+				setWindSpeed(response.data['current'].wind_speed)
+				setWeatherIcon(response.data['current'].weather_icons[0])
+				setWindDirection(response.data['current'].wind_dir)})
+	}
+
+	useEffect(hook, [])
+
 	return(
 		<div>
 			<p><b>Name:</b> {country.name}</p>
@@ -12,22 +35,40 @@ const PrintCountry = ({country}) => {
 					<li key={lang.name}>{lang.name}</li>)}
 			</ul>
 			<img alt="{country.name} flag"  src={country.flag} height='300' width='450' />
+			<p><b>Weather in {country.capital}:</b></p>
+			<p><b>Temperature:</b> {weather_temperature} ºC</p>
+			<img alt="temperature icon" src={weather_icon} />
+			<p><b>Wind:</b> {wind_speed} MPH in direction {wind_direction}</p>
+
 		</div>)
 }
 
 const Display = ({countries}) => {
+	const [country, setCountry] = useState('')
+
 
 	if(countries.length > 10){
 		return(
-			<p>Too many matches! Filter more your search</p>
+			<p>Too many matches! Filter more your search.</p>
 		)
 	}
 
 	if(countries.length > 1){
+		const handleShowCountry = (country) => {
+			console.log("Clicked with ", country)
+			setCountry(country)
+			return (
+				<PrintCountry key={country.name} country={country} />
+			)
+		}
+
 		return (
 			<div>
 			     {countries.map(country =>
-		    		<p key={country.name}><b>Name:</b> {country.name}</p>)}
+			     	<div key={country.name}>
+		    			<p><b>Name:</b> {country.name}</p>
+    					<button onClick={() => handleShowCountry(country)}>Show</button>
+					</div>)}
 			</div>
 		)
 	}
@@ -36,7 +77,7 @@ const Display = ({countries}) => {
 		return (
 			<div>
 		    	{countries.map(country =>
-	    			<PrintCountry key={country.name} country={country} />
+    				<PrintCountry key={country.name} country={country} />
 	    		)}
 			</div>
 		) 
